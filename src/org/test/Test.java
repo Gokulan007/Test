0f99a82b14b1ff79301cca715c9a14d524410f2b
Gokulan007 <gokulangopi5@gmail.com>
2022-06-14 12:18:20 +0530

    ok

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -3,7 +3,11 @@ package org.test;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("gopi");
+		System.out.println("gokulan");
+		System.out.println("maddy");
+		
 
 	}
 
