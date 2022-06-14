57544cea236654c3d7072dd2f8c31a7ec1d59546
Gokulan007 <gokulangopi5@gmail.com>
2022-06-14 12:32:45 +0530

    gopi code

--- src/org/test/Test.java
+++ src/org/test/Test.java
@@ -7,7 +7,8 @@ public class Test {
 		System.out.println("gopi");
 		System.out.println("gokulan");
 		System.out.println("maddy");
-		
+		System.out.println("2345678");
+		System.out.println("-09876543");
 
 	}
 
